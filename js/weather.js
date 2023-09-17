7f8c851142dc35904ccc5be5b703053e5b4a59fe
tomatojams <tomatojams@naver.com>
2023-09-17 23:57:26 +0900

    n

--- js/weather.js
+++ js/weather.js
@@ -1,13 +1,12 @@
-const API_KEY = "009279a52114f87f7a7e1142ea4d19ff7";
-
+const API_KEY = "70884f237363a4b09ec8fed86b4877f1";
 
 function onGeoOk(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
 
-    //console.log("You live in", lat, lon);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    fetch(url);
+  //console.log("You live in", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  fetch(url);
 }
 
 function onGeoError() {
